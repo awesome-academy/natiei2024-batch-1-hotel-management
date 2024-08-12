c0498270dcade11a8869aaadc9201dd4c19e406c
Ngansan <21521174@gm.uit.edu.vn>
2024-08-12 21:39:00 +0700

    created code HTML, CSS for bill for rent

--- HotelManagerment/com.spring.sample/src/com/spring/sample/configuration/SecurityConfig.java
+++ HotelManagerment/com.spring.sample/src/com/spring/sample/configuration/SecurityConfig.java
@@ -57,8 +57,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/login").permitAll()
 				.antMatchers("/signup").permitAll()
 				.antMatchers("/regulations").permitAll()
-				.antMatchers("/room-categories").permitAll() // Cho phép truy cập công khai vào /room-category để kiểm
-																// tra HTML
+				.antMatchers("/room-categories").permitAll()
+				.antMatchers("/BillForRent").permitAll() // Cho phép truy cập công khai vào /room-category để kiểm
 				.antMatchers("/revenues").permitAll() // Thay đổi từ HEAD
 				.antMatchers("/authorizations").permitAll()
 				.antMatchers(HttpMethod.POST, "/users").permitAll()
