8c49ddff5ba18064db09164dddf04b62783dc8e7
Nguyễn Phúc Khang <98502471+Khang2561@users.noreply.github.com>
2024-08-13 11:36:24 +0700

    code html css for navbar and roomcategory #79521 and #78997

---
+++ HotelManagerment/com.spring.sample/src/com/spring/sample/controller/RoomCategoriesController.java
@@ -0,0 +1,19 @@
+package com.spring.sample.controller;
+import java.util.Locale;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Controller
+@EnableWebMvc
+public class RoomCategoriesController {
+	private static final Logger logger = LoggerFactory.getLogger(RoomCategoriesController.class);
+	@GetMapping(value = "/room-categories")
+	public String index(Locale locale, Model model) {
+		logger.info("Requesting roomcategory form");
+		return "RoomCategory/index";
+	}
+}
